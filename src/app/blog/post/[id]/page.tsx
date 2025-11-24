e6180112aa154899ecd55f17cc1e94c95be4e105
jayanthi-ch98 <jayanthi.ch98@gmail.com>
2025-11-24 18:29:58 +0530

    Fixing deployment issues

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,23 @@
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
+import { notFound } from 'next/navigation';
 
 export default function Page({ params }: { params: { id: string } }) {
   const post = posts.find((post) => post.id === params.id);
+
+  if (!post) {
+    notFound();
+  }
+
   return (
     <>
       <h1>Post</h1>
-      <Post {...post} />
-    </>)
+      <Post
+        id={post.id}
+        title={post.title}
+        content={post.content}
+        date={post.date}
+      />
+    </>
+  );
 }
