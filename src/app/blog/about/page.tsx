6f2b9c74bbb13fa36266b870fb2b0902cd0e9fd0
jayanthi-ch98 <jayanthi.ch98@gmail.com>
2025-11-24 18:34:21 +0530

    Changing about me page

--- src/app/blog/about/page.tsx
+++ src/app/blog/about/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <>
       <h1 className="text-purple-800">About</h1>
-      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
+      <p>This is a sample nextjs blog project.</p>
     </>)
 
 }
